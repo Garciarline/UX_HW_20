1f86c23a5dfa643511c47c7ff090f4495b184892
Garciarline <88305122+Garciarline@users.noreply.github.com>
2022-03-13 03:53:55 -0500

    im going to bed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
 console.log("your page is loading")
-
-$( "#button" ). delay(function() {
-    $( "#button" ).grow({
-     width: "300px",
-     height: "300px", 
-      }, 1500 );
-  });
   
 
   
